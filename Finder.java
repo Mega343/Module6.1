cd07a51f092e2c9838cd08706de8bb115c40eeb7
Mega343 <mega9594@Rambler.ru>
2016-03-31 00:13:07 +0300

    Update Finder.java

--- Finder.java
+++ Finder.java
@@ -3,7 +3,7 @@ package com.goit.gojavaonline.module5;
 
 public class Finder {
 
-  public static int findMax(int[] name){
+  public static int findMax(int[] name) throws ArrayIndexOutOfBoundsException{
       int max = name[0];
         for(int i = 0; i < name.length; i++){
            max = Math.max(max,name [i]);
@@ -11,7 +11,7 @@ public class Finder {
         return max;
     }
 
-    public static int findMin(int[] name){
+    public static int findMin(int[] name) throws ArrayIndexOutOfBoundsException{
        int min = name[0];
         for(int i = 0; i < name.length; i++){
             min = Math.min(min,name[i]);
